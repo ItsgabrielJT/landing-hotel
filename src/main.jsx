0a466ee9cce78fafdbc5f32924185b740562e2db
ItsGabrielJT <gabrielasimbaya82@gmail.com>
2026-04-08 21:48:42 -0500

    feat: agregar SmoothScroller con Lenis y GSAP para desplazamiento suave

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,47 @@
-import { StrictMode } from 'react'
+import { StrictMode, useEffect } from 'react'
 import { createRoot } from 'react-dom/client'
+import gsap from 'gsap'
+import Lenis from '@studio-freight/lenis'
 import './index.css'
 import App from './App.jsx'
 
+function SmoothScroller({ children }) {
+  useEffect(() => {
+    const lenis = new Lenis({
+      duration: 1.2,
+      easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
+      direction: 'vertical',
+      gestureDirection: 'vertical',
+      smooth: true,
+      mouseMultiplier: 1,
+      smoothTouch: false,
+      touchMultiplier: 2,
+    })
+
+    lenis.on('scroll', ScrollTrigger.update) // if ScrollTrigger is used later
+
+    gsap.ticker.add((time) => {
+      lenis.raf(time * 1000)
+    })
+    
+    gsap.ticker.lagSmoothing(0)
+
+    return () => {
+      lenis.destroy()
+      gsap.ticker.remove(lenis.raf)
+    }
+  }, [])
+
+  return children
+}
+
+// Temporary patch to quiet missing ScrollTrigger if not installed/registered
+const ScrollTrigger = { update: () => {} };
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <SmoothScroller>
+      <App />
+    </SmoothScroller>
   </StrictMode>,
 )
